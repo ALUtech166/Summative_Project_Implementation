504eb389ed4729fe93c10c5d094cc55ac704fe4c
ALUtech166 <s.tassiga@alustudent.com>
2023-12-06 03:38:17 +0200

    Payment features done

---
+++ pages/payment.vue
@@ -0,0 +1,133 @@
+<template>
+    <div class=" tw-mx-auto tw-items-center tw-h-screen">
+        <div class="tw-flex tw-flex-col lg:tw-flex-row tw-justify-between tw-gap-8 tw-p-6">
+            <!-- Delivery Details Section -->
+            <div class="tw-flex-1">
+                <div class="tw-bg-white tw-rounded-md tw-shadow-md tw-p-6">
+                    <h3 class="tw-text-xl tw-font-semibold tw-mb-4">Delivery Details</h3>
+                    <form class="tw-space-y-4">
+                        <div>
+                            <label for="fullName" class="tw-block tw-font-semibold tw-mb-1">Full Name</label>
+                            <input id="fullName" type="text" v-model="fullName" placeholder="John Doe"
+                                class="tw-w-full tw-py-2 tw-px-3 tw-border tw-rounded" />
+                        </div>
+
+                        <div>
+                            <label for="address" class="tw-block tw-font-semibold tw-mb-1">Address</label>
+                            <input id="address" type="text" v-model="address" placeholder="123 Main Street, City"
+                                class="tw-w-full tw-py-2 tw-px-3 tw-border tw-rounded" />
+                        </div>
+
+                        <div>
+                            <label for="phone" class="tw-block tw-font-semibold tw-mb-1">Phone Number</label>
+                            <input id="phone" type="tel" v-model="phone" placeholder="123-456-7890"
+                                class="tw-w-full tw-py-2 tw-px-3 tw-border tw-rounded" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <!-- Payment Details Section -->
+            <div class="tw-flex-1">
+                <div class="tw-bg-white tw-rounded-md tw-shadow-md tw-p-6">
+                    <h3 class="tw-text-xl tw-font-semibold tw-mb-4">Payment Details</h3>
+                    <form @submit.prevent="submitPayment" class="tw-space-y-4">
+                        <!-- Payment Fields (similar to previous examples) -->
+
+                        <div>
+                            <label for="cardNumber" class="tw-block tw-font-semibold tw-mb-1">Card Number</label>
+                            <input id="cardNumber" type="text" v-model="cardNumber" placeholder="**** **** **** ****"
+                                class="tw-w-full tw-py-2 tw-px-3 tw-border tw-rounded" />
+                        </div>
+
+                        <!-- Expiry Date and CVV -->
+                        <div class="tw-flex tw-space-x-4">
+                            <div class="tw-flex-1">
+                                <label for="expiryDate" class="tw-block tw-font-semibold tw-mb-1">Expiry Date</label>
+                                <input id="expiryDate" type="text" v-model="expiryDate" placeholder="MM/YY"
+                                    class="tw-w-full tw-py-2 tw-px-3 tw-border tw-rounded" />
+                            </div>
+                            <div class="tw-flex-1">
+                                <label for="cvv" class="tw-block tw-font-semibold tw-mb-1">CVV</label>
+                                <input id="cvv" type="text" v-model="cvv" placeholder="***"
+                                    class="tw-w-full tw-py-2 tw-px-3 tw-border tw-rounded" />
+                            </div>
+                        </div>
+
+                        <!-- Cardholder Name -->
+                        <div>
+                            <label for="cardholderName" class="tw-block tw-font-semibold tw-mb-1">Cardholder Name</label>
+                            <input id="cardholderName" type="text" v-model="cardholderName" placeholder="John Doe"
+                                class="tw-w-full tw-py-2 tw-px-3 tw-border tw-rounded" />
+                        </div>
+
+                        <!-- Payment Options -->
+                        <div class="tw-flex tw-space-x-4">
+                            <div>
+                                <label class="tw-block tw-font-semibold tw-mb-1">Payment Options</label>
+                                <div class="tw-flex">
+                                    <a href="#" class="tw-text-blue-500 tw-hover:tw-underline">MTN Mobile Money</a>
+                                </div>
+                            </div>
+
+                            <div>
+                                <label class="tw-block tw-font-semibold tw-mb-1">&nbsp;</label>
+                                <div class="tw-flex">
+                                    <a href="#" class="tw-text-blue-500 tw-hover:tw-underline">Mastercard</a>
+                                </div>
+                            </div>
+
+                            <div>
+                                <label class="tw-block tw-font-semibold tw-mb-1">&nbsp;</label>
+                                <div class="tw-flex">
+                                    <a href="#" class="tw-text-blue-500 tw-hover:tw-underline">Visa</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Payment Button -->
+                        <button
+                            class="tw-group tw-bg-blue-800 tw-shadow-lg tw-rounded-lg tw-relative tw-h-12 tw-mt-4 tw-cursor-pointer hover:tw-transform hover:tw-translate-x-2 hover:tw-transition-transform hover:tw-duration-300 hover:tw-shadow-2xl-blue-600 hover:tw-shadow-2xl-lg tw-w-full tw-overflow-hidden tw-rounded-lg tw-text-lg tw-shadow-2xl-2xl">
+                            <div
+                                class="tw-absolute tw-inset-0 tw-w-full tw-bg-blue-800 tw--transition-all tw-duration-[250ms] tw-ease-out group-hover:tw-w-full">
+                            </div>
+                            <span
+                                class="tw-relative tw-inline-flex tw-items-center tw-gap-2 tw-text-white group-hover:tw-text-white">
+                                Pay Now
+
+                            </span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            // Delivery Details
+            fullName: '',
+            address: '',
+            phone: '',
+
+            // Payment Details (similar to previous example)
+        };
+    },
+    methods: {
+        submitPayment() {
+            // Implement actual payment processing logic here
+            console.log('Payment submitted!');
+            // You would typically integrate with a secure payment gateway in a real-world scenario
+        },
+    },
+};
+</script>
+  
+<style scoped>
+/* Add scoped styling here */
+</style>
+  
--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -9,12 +9,12 @@
 
       <div class="tw-w-1/2">
         <div>
-          <h1 class="tw-text-4xl tw-font-bold tw-mb-2">{{ product.name }}</h1>
-          <p class="tw-text-gray-600">${{ product.price.toFixed(2) }}</p>
+          <h1 class="tw-text-6xl tw-font-extrabold tw-mb-2">{{ product.name }}</h1>
           <p class="tw-mt-2 tw-text-md tw-text-gray-700 tw-text-justify">{{ product.description }}</p>
         </div>
+        <p class="tw-text-black tw-text-4xl tw-font-extrabold tw-mt-4">${{ product.price.toFixed(2) }}</p>
 
-        <div class="tw-flex tw-flex-col lg:tw-flex-row tw-gap-2 tw-px-2">
+        <div class="tw-flex tw-flex-col lg:tw-flex-row tw-gap-2">
           <button @click="addToCart"
             class="tw-group bg-hover tw-shadow-lg tw-rounded-lg tw-relative tw-h-12 tw-mt-2 tw-cursor-pointer hover:tw-transform hover:tw-duration-300 tw-w-48 tw-overflow-hidden tw-rounded-lg tw-text-lg tw-shadow-2xl">
             <div
@@ -26,14 +26,14 @@
             </span>
           </button>
 
-          <button
+          <button @click="$router.push(`/payment`)"
             class="tw-group bg-hover tw-shadow-lg tw-rounded-lg tw-relative tw-h-12 tw-mt-2 tw-cursor-pointer hover:tw-transform hover:tw-duration-300 tw-w-48 tw-overflow-hidden tw-rounded-lg tw-text-lg tw-shadow-2xl">
 
             <div
               class="tw-absolute tw-inset-0 tw-w-full tw-bg-red-800  tw--transition-all tw-duration-[250ms] tw-ease-out group-hover:tw-w-full">
             </div>
             <span class="tw-relative tw-inline-flex tw-items-center tw-gap-2 tw-text-white group-hover:tw-text-white">
-              Buy now
+              Pay now
 
             </span>
           </button>
@@ -64,11 +64,11 @@ export default {
       product: null,
 
       products: [
-        { id: 1, name: 'Product 1', price: 19.99, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: 'https://img.freepik.com/free-photo/white-pill-bottle-orange-pills-against-white-background_181624-839.jpg?w=740&t=st=1698871967~exp=1698872567~hmac=f30fe6a5d9d9727572221b21a319e2f1be6630db57215d90ab553696a9d2fd40' },
-        { id: 2, name: 'Product 2', price: 29.99, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: 'https://img.freepik.com/free-photo/white-pill-bottle-orange-pills-against-white-background_181624-839.jpg?w=740&t=st=1698871967~exp=1698872567~hmac=f30fe6a5d9d9727572221b21a319e2f1be6630db57215d90ab553696a9d2fd40' },
+        { id: 1, name: 'Product 1', price: 19.99, description: 'Our platform provides a seamless experience for individuals seeking medications by partnering with major insurance providers to ensure coverage and cost-effectiveness. Users can shop for a wide range of approved medications at competitive prices, offering convenience and accessibility. Our licensed pharmacists are readily available to address any concerns, providing information on pricing, allergies, side-effects, and more. As a comprehensive healthcare platform, we aim to simplify the process of acquiring quality medications, fostering a trusted space where users can confidently shop for their health needs. ', image: 'https://img.freepik.com/free-photo/white-pill-bottle-orange-pills-against-white-background_181624-839.jpg?w=740&t=st=1698871967~exp=1698872567~hmac=f30fe6a5d9d9727572221b21a319e2f1be6630db57215d90ab553696a9d2fd40' },
+        { id: 2, name: 'Product 2', price: 29.99, description: 'Our platform provides a seamless experience for individuals seeking medications by partnering with major insurance providers to ensure coverage and cost-effectiveness. Users can shop for a wide range of approved medications at competitive prices, offering convenience and accessibility. Our licensed pharmacists are readily available to address any concerns, providing information on pricing, allergies, side-effects, and more. As a comprehensive healthcare platform, we aim to simplify the process of acquiring quality medications, fostering a trusted space where users can confidently shop for their health needs. ', image: 'https://img.freepik.com/free-photo/white-pill-bottle-orange-pills-against-white-background_181624-839.jpg?w=740&t=st=1698871967~exp=1698872567~hmac=f30fe6a5d9d9727572221b21a319e2f1be6630db57215d90ab553696a9d2fd40' },
         // Add more products as needed
-        { id: 3, name: 'Product 3', price: 39.99, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: 'https://img.freepik.com/free-photo/white-pill-bottle-orange-pills-against-white-background_181624-839.jpg?w=740&t=st=1698871967~exp=1698872567~hmac=f30fe6a5d9d9727572221b21a319e2f1be6630db57215d90ab553696a9d2fd40' },
-        { id: 4, name: 'Product 4', price: 49.99, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: 'https://img.freepik.com/free-photo/white-pill-bottle-orange-pills-against-white-background_181624-839.jpg?w=740&t=st=1698871967~exp=1698872567~hmac=f30fe6a5d9d9727572221b21a319e2f1be6630db57215d90ab553696a9d2fd40' },
+        { id: 3, name: 'Product 3', price: 29.99, description: 'Our platform provides a seamless experience for individuals seeking medications by partnering with major insurance providers to ensure coverage and cost-effectiveness. Users can shop for a wide range of approved medications at competitive prices, offering convenience and accessibility. Our licensed pharmacists are readily available to address any concerns, providing information on pricing, allergies, side-effects, and more. As a comprehensive healthcare platform, we aim to simplify the process of acquiring quality medications, fostering a trusted space where users can confidently shop for their health needs. ', image: 'https://img.freepik.com/free-photo/white-pill-bottle-orange-pills-against-white-background_181624-839.jpg?w=740&t=st=1698871967~exp=1698872567~hmac=f30fe6a5d9d9727572221b21a319e2f1be6630db57215d90ab553696a9d2fd40' },
+        { id: 4, name: 'Product 4', price: 29.99, description: 'Our platform provides a seamless experience for individuals seeking medications by partnering with major insurance providers to ensure coverage and cost-effectiveness. Users can shop for a wide range of approved medications at competitive prices, offering convenience and accessibility. Our licensed pharmacists are readily available to address any concerns, providing information on pricing, allergies, side-effects, and more. As a comprehensive healthcare platform, we aim to simplify the process of acquiring quality medications, fostering a trusted space where users can confidently shop for their health needs. ', image: 'https://img.freepik.com/free-photo/white-pill-bottle-orange-pills-against-white-background_181624-839.jpg?w=740&t=st=1698871967~exp=1698872567~hmac=f30fe6a5d9d9727572221b21a319e2f1be6630db57215d90ab553696a9d2fd40' },
 
       ],
 
